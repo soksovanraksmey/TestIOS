a3afda470fc692ebe03e006f3d9390252851c7e5
zed <soksovanraksmey@gmail.com>
2021-09-12 22:49:48 +0700

    test commit again

--- TestIOS/ViewController.swift
+++ TestIOS/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello wold...")
+        print("hello wold...")
         // Do any additional setup after loading the view.
     }
 
